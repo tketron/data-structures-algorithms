7c104a671a0ed2265f9e0b7496fc8cc5d86c46c5
Tyler Ketron <tketron@gmail.com>
2018-09-23 18:29:12 -0400

    working LL with random pointer solution

--- leetcode/138-copy-list-with-random-pointer.js
+++ leetcode/138-copy-list-with-random-pointer.js
@@ -40,33 +40,19 @@ var copyRandomList = function(head) {
     newNode = nodes.get(node);
   }
 
-  console.log(nodes);
-
   return newHead;
-
-  // var newHead = copyNode(head);
-
-  // function copyNode(node) {
-  //   if (node === null) return null;
-  //   var nodeCopy = new RandomListNode(node.label);
-  //   nodeCopy.next = copyNode(node.next);
-  //   nodeCopy.random = copyNode(node.random);
-  //   return nodeCopy;
-  // }
-
-  // return newHead;
 };
 
-var node1 = new RandomListNode(1);
-var node2 = new RandomListNode(2);
-var node3 = new RandomListNode(3);
-var node4 = new RandomListNode(4);
+// var node1 = new RandomListNode(1);
+// var node2 = new RandomListNode(2);
+// var node3 = new RandomListNode(3);
+// var node4 = new RandomListNode(4);
 
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node1.random = node4;
-node2.random = node1;
-node4.random = node2;
+// node1.next = node2;
+// node2.next = node3;
+// node3.next = node4;
+// node1.random = node4;
+// node2.random = node1;
+// node4.random = node2;
 
-copyRandomList(node1);
+// copyRandomList(node1);
