5f862bf754eac9ed4ebb38c7472374ec0253c20f
Tyler Ketron <tketron@gmail.com>
2018-09-28 16:56:41 -0400

    completed excel column to number with tests

--- leetcode/171-excel-column-number.js
+++ leetcode/171-excel-column-number.js
@@ -9,13 +9,12 @@
  * @return {number}
  */
 var titleToNumber = function(s) {
-  // 64
   let count = 0;
-  let multiplier = 1;
+  let multiplier = 0;
 
   for (let i = s.length - 1; i >= 0; i--) {
-    count += multiplier * (s.charCodeAt(i) - 64);
-    multiplier += 25;
+    count += (s.charCodeAt(i) - 64) * 26 ** multiplier;
+    multiplier++;
   }
 
   return count;
--- leetcode/test/excel-column-number.test.js
+++ leetcode/test/excel-column-number.test.js
@@ -4,4 +4,5 @@ test('it converts column titles to numbers', () => {
   expect(titleToNumber('A')).toBe(1);
   expect(titleToNumber('AB')).toBe(28);
   expect(titleToNumber('ZY')).toBe(701);
+  expect(titleToNumber('AAA')).toBe(703);
 });
