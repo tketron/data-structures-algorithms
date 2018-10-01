823b7cd4293cb19415b78227629cae30e36ae59e
Tyler Ketron <tketron@gmail.com>
2018-09-30 20:49:11 -0700

    added sorted array to BST

---
+++ leetcode/108-convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,7 @@
+/**
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ *
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ *
+ */
